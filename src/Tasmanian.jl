8b2a4b3a9785038b338c3a0f0582e472471812f5
MichelJuillard <michel.juillard@mjui.fr>
2025-10-01 16:30:43 +0200

    export more functions

--- src/Tasmanian.jl
+++ src/Tasmanian.jl
@@ -14,20 +14,21 @@ include("libTasmanian.jl")
 include("TSG.jl")
 include("../examples/examples.jl")
 
-export TasmanianSG, LocalRules, compareGrids, copyGrid, differentiate!, enableAcceleration,
+export TasmanianSG, LocalRules, clearConformalTransform!, clearDomainTransform!, clearLevelLimits!, clearRefinement!, compareGrids, copyGrid,
+    differentiate, differentiate!, enableAcceleration!,
     estimateAnisotropicCoefficients, evaluate, evaluateBatch,
     evaluateBatch!, evaluateHierarchicalFunctions, evaluateSparseHierarchicalFunctions,
-    evaluateThreadSafe, getAlpha, getBeta, getConformalTransformASIN, getDims,
+    evaluateThreadSafe, getAlpha, getAccelerationType, getAnisotropicRefinement!, getBeta, getConformalTransformASIN, getDims,
     getDomainTransform, getHierarchicalCoefficients,
-    getInterpolationWeights, getInterpolationWeightsBatch, getGPUMemory, getLevelLimits, getLoadedPoints,
+    getInterpolationWeights, getInterpolationWeightsBatch, getGPUID, getGPUMemory, getHierarchicalSupport, getLevelLimits, getLoadedPoints, getLoadedValues,
     getNeededPoints, getNout, getNumDimensions, getGPUName, getNumGPUs, getNumLoaded,
     getNumNeeded, getNumOutputs, getNumPoints, getOrder, getPoints,
-    getQuadratureWeights, integrate, isAccelerationAvailable, isFourier, isGlobal,
+    getQuadratureWeights, getSurplusRefinement!, integrate, integrateHierarchicalFunctions, isAccelerationAvailable, isFourier, isGlobal,
     isLocalPolynomial, isSequence, isSetDomainTransform, isSetConformalTransformASIN,
     isWavelet,
-    loadNeededPoints!, loadNeededValues!, makeFourierGrid!, makeGlobalGrid!,
-    makeLocalPolynomialGrid!, makeSequenceGrid!, removePointsByHierarchicalCoefficient!, setAnisotropicRefinement!,
-    setConformalTransformASIN!, makeWaveletGrid!,
+    loadNeededPoints!, loadNeededValues!, makeFourierGrid, makeFourierGrid!, makeGlobalGrid, makeGlobalGrid!, makeLocalPolynomialGrid,
+    makeLocalPolynomialGrid!, makeSequenceGrid, makeSequenceGrid!, makeWaveletGrid, makeWaveletGrid!, mergeRefinement!,
+    removePointsByHierarchicalCoefficient!, setAnisotropicRefinement!, setConformalTransformASIN!, makeWaveletGrid!,
     setDomainTransform!, setGPUID!, setHierarchicalCoefficients!, setSurplusRefinement!, updateFourierGrid!, updateGlobalGrid!,
     updateLocalPolynomialGrid!, updateSequenceGrid!, updateWaveletGrid!
 
