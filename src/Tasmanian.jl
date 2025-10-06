155dda7133522d9082ec2551a97eaf52beb79d9f
MichelJuillard <michel.juillard@mjui.fr>
2025-10-06 18:32:24 +0200

    add exported symbols

--- src/Tasmanian.jl
+++ src/Tasmanian.jl
@@ -2,7 +2,7 @@ module Tasmanian
 
 import Base: show, read, run, write
 using LinearAlgebra
-using Plots
+using RecipesBase
 using Random
 using SparseArrays
 using Tasmanian_jll
@@ -11,21 +11,23 @@ const global TASlib = libtasmaniansparsegrid
 
 # includes
 include("libTasmanian.jl")
+include("plot.jl")
 include("TSG.jl")
 include("../examples/examples.jl")
 
-export TasmanianSG, LocalRules, clearConformalTransform!, clearDomainTransform!, clearLevelLimits!, clearRefinement!, compareGrids, copyGrid,
+export TasmanianSG, beginConstruction, clearConformalTransform!, clearDomainTransform!, clearLevelLimits!, clearRefinement!, compareGrids, copyGrid,
     differentiate, differentiate!, enableAcceleration!,
     estimateAnisotropicCoefficients, evaluate, evaluateBatch,
     evaluateBatch!, evaluateHierarchicalFunctions, evaluateSparseHierarchicalFunctions,
-    evaluateThreadSafe, getAlpha, getAccelerationType, getAnisotropicRefinement!, getBeta, getConformalTransformASIN, getDims,
+    evaluateThreadSafe, finishConstruction!, getAlpha, getAccelerationType, getAnisotropicRefinement!, getBeta, getCandidateConstructionPoints,
+    getCandidateConstructionPointsSurplus, getConformalTransformASIN, getCustomRuleDescription, getDims,
     getDomainTransform, getHierarchicalCoefficients,
     getInterpolationWeights, getInterpolationWeightsBatch, getGPUID, getGPUMemory, getHierarchicalSupport, getLevelLimits, getLoadedPoints, getLoadedValues,
     getNeededPoints, getNout, getNumDimensions, getGPUName, getNumGPUs, getNumLoaded,
     getNumNeeded, getNumOutputs, getNumPoints, getOrder, getPoints,
-    getQuadratureWeights, getSurplusRefinement!, integrate, integrateHierarchicalFunctions, isAccelerationAvailable, isFourier, isGlobal,
-    isLocalPolynomial, isSequence, isSetDomainTransform, isSetConformalTransformASIN,
-    isWavelet,
+    getQuadratureWeights, getRule, getSurplusRefinement!, integrate, integrateHierarchicalFunctions, isAccelerationAvailable, isFourier, isGlobal,
+    isLocalPolynomial, isSequence, isSetDomainTransform, isSetConformalTransformASIN, isUsingConstruction, 
+    isWavelet, loadConstructedPoint!,
     loadNeededPoints!, loadNeededValues!, makeFourierGrid, makeFourierGrid!, makeGlobalGrid, makeGlobalGrid!, makeLocalPolynomialGrid,
     makeLocalPolynomialGrid!, makeSequenceGrid, makeSequenceGrid!, makeWaveletGrid, makeWaveletGrid!, mergeRefinement!,
     removePointsByHierarchicalCoefficient!, setAnisotropicRefinement!, setConformalTransformASIN!, makeWaveletGrid!,
